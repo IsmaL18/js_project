e0d1034c09fcb8ecd4b58c7d14a0dc5e7afe5895
IsmaL18 <ismaeldebbagh8@gmail.com>
2023-06-07 17:26:05 +0200

    Ajout du fichier dataset

---
+++ index.js
@@ -0,0 +1,11 @@
+const resultList = [
+    {number_bib: '1234', gender: 'M', result: '1:34.2', country:'kenya', age : '20'},
+    {number_bib: '1235', gender: 'F', result: '1:37.2', country:'france', age : '370'},
+    {number_bib: '1239', gender: 'M', result: '1:42.2', country:'usa',age : '18'},
+    {number_bib: '1237', gender: 'F', result: '2:34.5', country:'france', age : '60'},
+    {number_bib: '1232', gender: 'F', result: '3:13.2', country:'kenya', age : '50'},
+    {number_bib: '1999', gender: 'F', result: '1:15.1', country:'usa', age : '46'}
+];
+
+// RANKING FUNCTIONS PART
+
