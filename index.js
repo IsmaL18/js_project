25ac218b55118578dc39869b38a484062daa3523
leoo83 <hanifi.leo@gmail.com>
2023-06-08 11:36:44 +0200

    modif

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const resultList = [
-    {number_bib: '1234', gender: 'M', result: '1:34.2', country:'kenya', age : '20'},
+    {number_bib: '', gender: 'M', result: '1:34.2', country:'kenya', age : '20'},
     {number_bib: '1235', gender: 'F', result: '1:37.2', country:'france', age : '370'},
     {number_bib: '1239', gender: 'M', result: '1:42.2', country:'usa',age : '18'},
     {number_bib: '1237', gender: 'F', result: '2:34.5', country:'france', age : '60'},
