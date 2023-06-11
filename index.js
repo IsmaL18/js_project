da806aa98f2006f60b522a23a6bbe039a2bd0138
leoo83 <hanifi.leo@gmail.com>
2023-06-11 17:42:29 +0200

    modif

--- index.js
+++ index.js
@@ -3,84 +3,68 @@ const R = require('ramda');
 
 const filePath = 'Dataset-Boston-2019.csv';
 
-const stringToTimeInMinutes = (s) => {
-    const [hour, min] = s.split(':');
-    return Number(hour) * 60 + Number(min);
-};
 
-const getSortedList = R.pipe (
-    R.map(
-        R.when(
-            R.has('result'),
-            R.over(R.lensProp('result'), stringToTimeInMinutes)
-        )
-    ),
-    R.sortBy(R.prop('result'))
-);
+// Lecture du fichier
 
-const getRankingMen = R.pipe(
-    R.filter(x => x.gender === 'M'),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingMen(resultList)
+const data = fs.readFileSync(filePath, 'utf8');
 
-const getRankingWomen = R.pipe(
-    R.filter(x => x.gender === 'F'),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation:
-
-const getRankingByAge = (minAge, maxAge) => R.pipe(
-    R.filter(R.both((x => minAge <= parseInt(x.age)),(x => parseInt(x.age) <= maxAge))),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingByCountry('kenya')(resultList)
-
-const getRankingByCountry = (country) => R.pipe(
-    R.filter(x => x.country === country),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingByAge(0, 20)(resultList) renvoie le classement des participants qui ont entre 0 et 20 ans
-
-
-fs.readFile(filePath, 'utf8', (error, data) => {
-    if (error) {
-        console.error('Une erreur s\'est produite lors du chargement du dataset :', error);
-        return;
+// Convertir le CSV en tableau d'objets
+const rows = data.split('\n');
+const headers = rows[0].split(',');
+const dataset = rows.slice(1).map(row => {
+    const values = row.split(',');
+    if (values.length === headers.length) {
+        return headers.reduce((obj, header, index) => {
+            obj[header.trim()] = values[index].trim();
+            return obj;
+        }, {});
     }
+}).filter(Boolean);
 
-    // Convertir le CSV en tableau d'objets
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-    const dataset = rows.slice(1).map(row => {
-        const values = row.split(',');
-        if (values.length === headers.length) {
-            return headers.reduce((obj, header, index) => {
-                obj[header.trim()] = values[index].trim();
-                return obj;
-            }, {});
-        }
-    }).filter(Boolean);
-
-    // Générer un numéro de dossard aléatoire pour chaque participant
-    const totalParticipants = dataset.length;
-    const datasetWithRandomNumberBib = dataset.map(participant => {
-        participant.number_bib = Math.floor(Math.random() * totalParticipants) + 1;
-        return participant;
-    });
-
-    // Créer le tableau resultList avec les propriétés spécifiées, y compris number_bib
-    const resultList = datasetWithRandomNumberBib.map(participant => {
-        return {
-            gender: participant.Gender,
-            result_hr: participant.Result_hr,
-            country: participant.Country,
-            age: participant.Age,
-            number_bib: participant.number_bib
-        };
-    });
+// Générer un numéro de dossard aléatoire pour chaque participant
+const totalParticipants = dataset.length;
+const datasetWithRandomNumberBib = dataset.map(participant => {
+    participant.number_bib = Math.floor(Math.random() * totalParticipants) + 1;
+    return participant;
 });
+
+// Générer un tableau d'indices uniques
+const indices = Array.from(Array(totalParticipants).keys());
+
+// Mélanger les indices de manière aléatoire
+for (let i = indices.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [indices[i], indices[j]] = [indices[j], indices[i]];
+}
+
+// Attribuer les number_bib uniques aux participants
+const datasetWithUniqueNumberBib = datasetWithRandomNumberBib.map((participant, index) => {
+    participant.number_bib = indices[index] + 1;
+    return participant;
+});
+
+// Créer le tableau resultList avec les propriétés spécifiées, y compris number_bib
+const resultList = datasetWithUniqueNumberBib.map(participant => {
+    return {
+        gender: participant.Gender,
+        result_hr: participant.Result_hr,
+        country: participant.Country,
+        age: participant.Age,
+        number_bib: participant.number_bib
+    };
+});
+
+
+
+// Utiliser le tableau resultList ou effectuer d'autres opérations avec les données
+console.log(resultList);
+
+
+const numberBibs = resultList.map(participant => participant.number_bib);
+const isUnique = numberBibs.length === new Set(numberBibs).size;
+
+console.log("Les number_bib sont uniques :", isUnique);
+// Mélanger le tableau resultList de manière aléatoire
+let shuffledResultList = resultList.sort(() => Math.random() - 0.5);
+
+console.log(shuffledResultList);
