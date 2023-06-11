69910fa207ffad67364af347547a7e8bbff2b37e
IsmaL18 <ismaeldebbagh8@gmail.com>
2023-06-11 19:20:18 +0200

    New modif

--- index.js
+++ index.js
@@ -3,49 +3,62 @@ const R = require('ramda');
 
 const filePath = 'Dataset-Boston-2019.csv';
 
-const stringToTimeInMinutes = (s) => {
-    const [hour, min] = s.split(':');
-    return Number(hour) * 60 + Number(min);
+function processResultList(resultList) {
+    /*const resultList2 = [
+        {number_bib: '1234', gender: 'M', result: '1:34.2', country:'kenya' , age : '20'},
+        {number_bib: '1235', gender: 'F', result: '1:37.2', country:'kenya' ,age : '20'},
+        {number_bib: '1239', gender: 'M', result: '1:42.2', country:'kenya' ,age : '20'},
+        {number_bib: '1237', gender: 'F', result: '2:34.5',country:'kenya', age : '20'},
+        {number_bib: '1232', gender: 'F', result: '3:13.2',country:'kenya' ,age : '20'},
+        {number_bib: '1999', gender: 'F', result: '1:15.1',country:'kenya', age : '20'}
+    ];*/
+
+    const stringToTimeInMinutes = (s) => {
+        const [hour, min] = s.split(':');
+        return Number(hour) * 60 + Number(min);
+    };
+
+    const getSortedList = R.pipe(
+        R.map(
+            R.when(
+                R.has('result'),
+                R.over(R.lensProp('result'), stringToTimeInMinutes)
+            )
+        ),
+        R.sortBy(R.prop('result'))
+    );
+
+    const getRankingMen = R.pipe(
+        R.filter(x => x.gender === 'M'),
+        getSortedList,
+        R.head(10)
+    );
+    // exemple d'utilisation: getRankingMen(resultList)
+
+    const getRankingWomen = R.pipe(
+        R.filter(x => x.gender === 'F'),
+        getSortedList,
+        R.head(10)
+    );
+    // exemple d'utilisation: getRankingWomen(resultList)
+
+    const getRankingByAge = (minAge, maxAge) => R.pipe(
+        R.filter(R.both((x => minAge <= parseInt(x.age)), (x => parseInt(x.age) <= maxAge))),
+        getSortedList,
+        R.head(10)
+    );
+    // exemple d'utilisation: getRankingByCountry('kenya')(resultList)
+
+    const getRankingByCountry = (country) => R.pipe(
+        R.filter(x => x.country === country),
+        getSortedList,
+        R.head(10)
+    );
+    // exemple d'utilisation: getRankingByAge(0, 20)(resultList) renvoie le classement des participants qui ont entre 0 et 20 ans
+
+    console.log(resultList);
+    console.log(getRankingMen(resultList));
 };
-
-const getSortedList = R.pipe (
-    R.map(
-        R.when(
-            R.has('result'),
-            R.over(R.lensProp('result'), stringToTimeInMinutes)
-        )
-    ),
-    R.sortBy(R.prop('result'))
-);
-
-const getRankingMen = R.pipe(
-    R.filter(x => x.gender === 'M'),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingMen(resultList)
-
-const getRankingWomen = R.pipe(
-    R.filter(x => x.gender === 'F'),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation:
-
-const getRankingByAge = (minAge, maxAge) => R.pipe(
-    R.filter(R.both((x => minAge <= parseInt(x.age)),(x => parseInt(x.age) <= maxAge))),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingByCountry('kenya')(resultList)
-
-const getRankingByCountry = (country) => R.pipe(
-    R.filter(x => x.country === country),
-    getSortedList,
-    R.head(10)
-);
-// exemple d'utilisation: getRankingByAge(0, 20)(resultList) renvoie le classement des participants qui ont entre 0 et 20 ans
-
 
 fs.readFile(filePath, 'utf8', (error, data) => {
     if (error) {
@@ -77,10 +90,15 @@ fs.readFile(filePath, 'utf8', (error, data) => {
     const resultList = datasetWithRandomNumberBib.map(participant => {
         return {
             gender: participant.Gender,
-            result_hr: participant.Result_hr,
+            result: participant.Result_hr,
             country: participant.Country,
             age: participant.Age,
             number_bib: participant.number_bib
         };
     });
+
+
+
+    processResultList(resultList);
 });
+
